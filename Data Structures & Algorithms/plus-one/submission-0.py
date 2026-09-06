a1dae640b187415b9e27bd97323d9dd3a7a316b3
kururu <45058799+kururu-abdo@users.noreply.github.com>
2026-09-06 09:49:02 +0300

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if not digits:
+            return []
+        s=""
+        for num in digits:
+            s += str(num)
+        sum = int(s)+1
+        
+        
+
+
+
+        result = list(str(sum))
+        
+
+        
+
+
+
+
+        return result
